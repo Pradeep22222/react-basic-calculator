bd824065570e85c7540dc6b0ab985ccebeee17a2
PK <pradeepdhital124@gmail.com>
2022-12-03 13:22:50 +1100

    num a and num b is grabbed and state is updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
+import { useState } from 'react';
 import './App.css';
 import { ButtonArea } from './components/ButtonArea';
 import { Display } from './components/Display';
 
 function App() {
+
   return (
     <div className="app">
-      <ButtonArea></ButtonArea>
+      <ButtonArea
+      ></ButtonArea>
       <p>=</p>
       <Display></Display>
     </div>
--- src/components/ButtonArea.js
+++ src/components/ButtonArea.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 
 export const ButtonArea = () => {
+  const [numA, setNumA] = useState("");
+  const [numB, setNumB] = useState("");
+  const onNumAChange = (e) => {
+    e.preventDefault();
+    const { value } = e.target;
+    setNumA(value);
+  };
+  const onNumBChange = (e) => {
+    e.preventDefault();
+    const { value } = e.target;
+    setNumB(value);
+  };
+  
   return (
     <div>
-      <Form.Control type="number" placeholder="enter number" />
+      <Form.Control
+        type="number"
+        placeholder="enter number"
+        onChange={onNumAChange}
+      />
       <Form.Select aria-label="Default select example">
         <option>Select the operator</option>
         <option value="+">+</option>
@@ -12,7 +29,11 @@ export const ButtonArea = () => {
         <option value="*">*</option>
         <option value="/">/</option>
       </Form.Select>
-      <Form.Control type="number" placeholder="enter number" />
+      <Form.Control
+        type="number"
+        placeholder="enter number"
+        onChange={onNumBChange}
+      />
     </div>
   );
-}
+};
